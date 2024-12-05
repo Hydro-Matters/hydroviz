c767f87babb66ff3723e22d60aa730bd26c9b98a
Kevin Larnier <kevin.larnier@hydro-matters.fr>
2024-12-05 11:42:56 +0100

    Add SWOT (nc) observations set IO + reaches maps

--- swotdawgviz/io/__init__.py
+++ swotdawgviz/io/__init__.py
@@ -5,3 +5,4 @@ from .hivdi import *
 from .h2ivdi import *
 from .sic4dvar import *
 from .discharge_algorithm_results import DischargeAlgorithmResults
+from .swot_observations_set import SwotObservationsSet
